c95eb5144cf470918ede75d9fbe022a382a4fb57
Raj Das <mail.rajdas@gmail.com>
2018-02-21 00:16:29 +0530

    removing old files

--- about.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'inc/header.php';
-require 'inc/nav.php';?>
-
-<div class="fb-comments" data-href="" data-width="500" data-numposts="10"></div>
-<div id="fb-root"></div>
-<script type="text/javascript" src="js/sdk.js"></script>
-<script type="text/javascript" src="js/comment.js"></script>
-<?php require 'inc/footer.php' ?>
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'inc/header.php';
-require 'inc/nav.php';?>
-<script type="text/javascript" src="js/dot.js"></script>
-<?php require_once 'inc/footer.php';?>
