88e162c5b153230d070e6c805459c33cc11109df
rajdas <mail.rajdas@gmail.com>
2019-09-09 18:26:45 +0530

    profile modification

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var routes = require('./routes/index');
 var app =  express();
+var bodyParser=require('body-parser');
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,10 +10,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser());
 
 
-app.use('/', routes);
-app.use('/sitemap', routes);
+app.use(routes);
 
 
 module.exports = app;
